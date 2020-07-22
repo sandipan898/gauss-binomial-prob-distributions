ad55247c6e9a8b8f9f03dc7035ecb0349947e69c
sandipan898 <sandipan.das898@gmail.com>
2020-07-22 08:24:53 +0530

    preparing to upload

---
+++ prob-distributions/README.md
@@ -0,0 +1,9 @@
+# prob-distribution
+
+### Summary of the package
+
+Package to implement probability distribution functions and visualization of a dataset
+
+### Installation
+
+pip install prob-distribution
--- setup.py
+++ setup.py
@@ -1,7 +1,9 @@
 from setuptools import setup
 
 setup(name='prob-distributions',
-      version='0.1',
-      description='Gaussian distributions',
+      version='1.0',
+      description='Gaussian and Binomial distributions',
       packages=['prob-distributions'],
+      author='Sandipan Das',
+      author_email='sandipan.das898@gmail.com',
       zip_safe=False)
